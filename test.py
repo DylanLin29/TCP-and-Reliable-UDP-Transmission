fe946f970a8f5894d6077ef3067e6156d97526cb
Dylan Lin <Hongzhen_Lin@ucsb.edu>
2020-04-14 17:41:24 -0700

    update directory

--- test.py
+++
@@ -1,8 +0,0 @@
-import ipaddress
-import socket
-from socket import AF_INET, SOCK_STREAM
-
-ipaddress.ip_address('127.0.0.123')
-
-clientSocket = socket.socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(('127.0.0.123', 1234))
